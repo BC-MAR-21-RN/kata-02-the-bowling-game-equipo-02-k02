d9fc3caa1606ef85d7c5b49797ea9031a6d7b631
Demis Rincon <darmfma@gmail.com>
2021-03-17 12:43:08 -0600

    Feat: function to to get points after every shot
    Co-authored-by: José Támara <jkzasori@users.noreply.github.com>
    Co-authored-by:Pavel Macias
    <PavelMacias05@users.noreply.github.com>
    Co-authored-by: Pavel Macias
    <Isaac0vk@users.noreply.github.com>

--- js/GetAdditionalData.js
+++ js/GetAdditionalData.js
@@ -4,9 +4,77 @@ function GetAdditionalData(score) {
     this.score = score
     this.score.map((item, index) => {
   
+        const cumulativeTotal = () => {
+            if (index > 0) {
+                try {
+                    return this.score[index - 1].points
+                } catch (error) {
+                    return "no exist"
+                }
+            }
+            else {
+
+                return 0
+            }
+        }
+
+        const nextFirstShot = () => {
+            if (index <= 8) {
+                try {
+                    return this.score[index + 1].shot1
+                } catch (error) {
+                    return "no exist"
+                }
+            }
+            return 0
+        }
+
+        const nextTotal = () => {
+            if (index <= 8) {
+                try {
+                    return this.score[index + 1].total
+                } catch (error) {
+                    return "no exist"
+                }
+            }
+            return 0
+        }
+        const next2Total = () => {
+            if (index <= 7) {
+                try {
+                    return this.score[index + 2].total
+                } catch (error) {
+                    return "no exist"
+                }
+            }
+            return 0
+        }
+
+        const addition = nextFirstShot()
+        const cumulative = cumulativeTotal()
+        const nextTot = nextTotal()
+        const next2Tot = next2Total()
 
 
-        return item
+        if (item.shot1 === 10 ) {
+            if (nextTot === "no exist" || next2Tot === "no exist" || cumulative === "no exist") {
+                return item.points = ""
+            }
+            return item.points = cumulative + item.total + nextTot + next2Tot
+        }
+        else if (item.total === 10|| item.shot2 === 10) {
+            if (addition === "no exist" || cumulative === "no exist") {
+                return item.points = ""
+            }
+            return item.points = cumulative + item.total + addition
+        }
+        else {
+            if (cumulative === "no exist") {
+                return item.points = ""
+            }
+            return item.points = cumulative + item.total
+        }
+   
 
     })
 
